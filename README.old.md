631db900c5c0c352de20a52233d41edbb7e480d8
Omar El-Hawary <Omar.El-Hawary@ibm.com>
2020-05-12 00:32:58 +0200

    project initialized

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# to-do
+DarkMatter assignment
---
+++ src/App.js
@@ -0,0 +1,42 @@
+import 'carbon-components/scss/globals/scss/styles.scss';
+
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import { Content, Header, HeaderName } from 'carbon-components-react';
+
+function App() {
+  return (
+    <div className="App">
+      <Header aria-label="IBM Platform Name">
+        <HeaderName href="#" prefix="Your">
+          App
+        </HeaderName>
+      </Header>
+
+      <Content>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React</a> - <a
+          className="App-link"
+          href="https://www.carbondesignsystem.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Carbon Design System
+        </a>
+      </Content>
+    </div>
+  );
+}
+
+export default App;
