e899c72885fccd6254a61b8e16308841ecd27d09
Omar El-Hawary <Omar.El-Hawary@ibm.com>
2020-05-13 11:38:19 +0200

    Title structure and decoration

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Header, HeaderName } from 'carbon-components-react';
 
 class Title extends Component {
   render() {
     return (
-      <Header aria-label="IBM Platform Name">
-        <Link>
-          <HeaderName prefix="">{this.props.title}</HeaderName>
-        </Link>
-      </Header>
+      <Link style={{ color: 'inherit', textDecoration: 'inherit' }}>
+        {this.props.title}
+      </Link>
     );
   }
 }
